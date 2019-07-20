14e2c1ea5f0568cce4a46762dc45400256b0c183
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-07-20 13:25:08 -0700

    starting component architecture

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      string:'Hello Colin'
+      monsters: [
+        {
+          name: 'Frankenstein',
+          id: '1'
+        },
+        {
+          name: 'Dracula',
+          id: '2'
+        },
+        {
+          name: 'Zombie',
+          id: '3'
+        }
+      ]
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(users => this.setState({ monsters: users}))
   }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{this.state.string}</p>
-          <button onClick={() => this.setState({ string: 'Hello test' })}>Press Here</button>
-        </header>
+        {
+          this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>
+        ))}
       </div>
     );
   }
