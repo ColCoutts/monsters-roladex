cc1d9d149b8fe17c1e80634739114f1c5b113678
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-07-20 16:11:48 -0700

    Updates

---
+++ precache-manifest.e4bd5b31dbc706000a7fe0fd63b9f257.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0b2476d25a250087248fbd73c641f82",
+    "url": "/monsters-roladex/index.html"
+  },
+  {
+    "revision": "a7ea7c43a2b3670f49c2",
+    "url": "/monsters-roladex/static/css/main.671ff3e4.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-roladex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "a7ea7c43a2b3670f49c2",
+    "url": "/monsters-roladex/static/js/main.b80d3867.chunk.js"
+  },
+  {
+    "revision": "66865d593b6d4b64d8eb",
+    "url": "/monsters-roladex/static/js/runtime~main.926f003c.js"
+  }
+]);
