ede1f2af45527fc80975c8fb56e5580ae90e2094
COLIN COUTTS <COUTTSTCOLIN@GMAIL.COM>
2019-07-20 13:41:34 -0700

    adding styles moving component duties

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
 
 class App extends Component {
   constructor() {
@@ -28,12 +29,11 @@ class App extends Component {
       .then(users => this.setState({ monsters: users}))
   }
   render() {
+    const { monsters } = this.state;
+
     return (
       <div className="App">
-        {
-          this.state.monsters.map(monster => (
-            <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+      <CardList monsters={monsters} />
       </div>
     );
   }
